34157cd7e017af8689c1896d8b03e22587b20993
aurelija-93 <aurelija.bellerby@gmail.com>
2023-11-05 12:51:34 +0000

    can view search result

--- lib/main.dart
+++ lib/main.dart
@@ -148,32 +148,25 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        home: const MyHomePage(title: 'Cocktail Carousel'),
       ),
     );
   }
 }
 
 class MyAppState extends ChangeNotifier {
-  Future<Drink>? currentDrink;
+  Future<Drink>? newDrink;
+  Drink? currentDrink;
 
   void getDrink() {
-    currentDrink = fetchDrink();
+    newDrink = fetchDrink();
+    newDrink!.then((value) => currentDrink = value);
     notifyListeners();
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -184,26 +177,21 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var appState = context.watch<MyAppState>();
+    Widget view;
+    switch (appState.currentDrink) {
+      case null:
+        view = const MainMenu();
+      case != null:
+        view = const SearchResult();
+      default:
+        throw UnimplementedError('no widget available');
+    }
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: const Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: MainMenu()),
+      body: Center(
+          child: Container(
+        child: view,
+      )),
     );
   }
 }
@@ -229,9 +217,69 @@ class MainMenu extends StatelessWidget {
           ],
         ),
         ElevatedButton(
-          onPressed: appState.getDrink,
+          onPressed: () {
+            appState.getDrink();
+          },
           child: const Text("I'd like a drink"),
         ),
+      ],
+    );
+  }
+}
+
+class SearchResult extends StatelessWidget {
+  const SearchResult({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var drink = appState.currentDrink;
+
+    return Column(
+      children: [
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Cocktail Carousel',
+            ),
+            Text(
+              'LOGO',
+            ),
+          ],
+        ),
+        DrinkCard(drink: drink),
+        ElevatedButton(
+          onPressed: () {
+            print(drink!.name);
+          },
+          child: const Text('View recipe'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            appState.getDrink();
+          },
+          child: const Text('Next drink'),
+        ),
+      ],
+    );
+  }
+}
+
+class DrinkCard extends StatelessWidget {
+  const DrinkCard({
+    super.key,
+    required this.drink,
+  });
+
+  final Drink? drink;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image(image: NetworkImage(drink!.image)),
+        Text(drink!.name),
       ],
     );
   }
